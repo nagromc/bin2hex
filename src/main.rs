f83067dd6f9d420ec0a000b0b062b87e2ca7083c
Morgan Courbet <github@sub.ploki.fr>
2022-06-11 11:10:45 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use clap::Parser;
+use std::{fs, path::Path, process};
+
+/// Converts a binary file to its hex representation
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Path of the input file
+    input_file: String,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let file_path_str = args.input_file.as_str();
+    validate_arguments(file_path_str);
+
+    let bytes = get_file_content(file_path_str);
+    let result = hex::encode(bytes);
+    println!("{}", result);
+}
+
+fn validate_arguments(file_path_str: &str) {
+    let file_path = Path::new(file_path_str);
+    if !file_path.exists() {
+        print!("The given file path [{}] doesn't exist", file_path_str);
+        process::exit(exitcode::DATAERR);
+    }
+    if !file_path.is_file() {
+        print!("The given file path [{}] is not a file", file_path_str);
+        process::exit(exitcode::DATAERR);
+    }
+}
+
+fn get_file_content(path: &str) -> Vec<u8> {
+    fs::read(&path).expect("Cannot read file")
+}
