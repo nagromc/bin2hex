fb3e196fb2c5e3634fbcfbc3ea52e7c2a818fcca
Morgan Courbet <github@sub.ploki.fr>
2022-07-25 17:55:22 +0200

    feat: allow to give an output path

--- src/main.rs
+++ src/main.rs
@@ -7,31 +7,72 @@ use std::{fs, path::Path, process};
 struct Args {
     /// Path of the input file
     input_file: String,
+    /// Path of the converted file if set. Otherwise, will output to `stdout`.
+    output_file: Option<String>,
 }
 
 fn main() {
     let args = Args::parse();
 
-    let file_path_str = args.input_file.as_str();
-    validate_arguments(file_path_str);
+    let input_file_path_str: String = args.input_file;
+    let output_file_path_str: Option<String> = args.output_file;
+    validate_arguments(&input_file_path_str, &output_file_path_str);
 
-    let bytes = get_file_content(file_path_str);
-    let result = hex::encode(bytes);
-    println!("{}", result);
+    let bytes = get_file_content(input_file_path_str);
+    let hex_result = hex::encode(bytes);
+    write_result(hex_result, output_file_path_str);
 }
 
-fn validate_arguments(file_path_str: &str) {
-    let file_path = Path::new(file_path_str);
-    if !file_path.exists() {
-        eprintln!("The given file path [{}] doesn't exist", file_path_str);
+fn write_result(hex_result: String, output_file_path_str: Option<String>) {
+    let file_path_string = output_file_path_str.unwrap();
+    let file_path_str = file_path_string.as_str();
+    match fs::write(&file_path_str, hex_result) {
+        Ok(_) => {}
+        Err(_) => {eprintln!("Could not write result in file {}", &file_path_str)}
+    };
+}
+
+fn validate_arguments(input_file_path_str: &String, output_file_path_str: &Option<String>) {
+    validate_input_file_path(input_file_path_str);
+    validate_output_file_path(output_file_path_str);
+}
+
+fn validate_input_file_path(path_string: &String) {
+    let path = Path::new(path_string.as_str());
+    if !path.exists() {
+        eprintln!("The given input file path [{}] doesn't exist", path_string);
         process::exit(exitcode::DATAERR);
     }
-    if !file_path.is_file() {
-        eprintln!("The given file path [{}] is not a file", file_path_str);
+    if !path.is_file() {
+        eprintln!("The given input file path [{}] is not a file", path_string);
         process::exit(exitcode::DATAERR);
     }
 }
 
-fn get_file_content(path: &str) -> Vec<u8> {
+fn validate_output_file_path(path_str: &Option<String>) {
+    if path_str.is_none() {
+        return;
+    }
+
+    let path = Path::new(path_str.as_ref().unwrap().as_str());
+    if path.exists() {
+        eprintln!("The given output file path [{}] already exists", path_str.as_ref().unwrap());
+        process::exit(exitcode::DATAERR);
+    }
+    match path.parent() {
+        None => {
+            eprintln!("The output file cannot be created at the given path [{}]", path_str.as_ref().unwrap());
+            process::exit(exitcode::DATAERR);
+        }
+        Some(path) => {
+            if !path.exists() | !path.is_dir() {
+                eprintln!("The output given path [{}] is not an existing dir", path.to_str().unwrap());
+                process::exit(exitcode::DATAERR);
+            }
+        }
+    }
+}
+
+fn get_file_content(path: String) -> Vec<u8> {
     fs::read(&path).expect("Cannot read file")
 }
